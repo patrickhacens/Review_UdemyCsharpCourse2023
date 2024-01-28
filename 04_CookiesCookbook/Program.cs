846d4a5744f0055d0eae00384a32aa4d017291e5
Moaad Amer <moaad02@gmail.com>
2024-01-28 17:10:45 +0200

    add global try catch

--- 04_CookiesCookbook/Program.cs
+++ 04_CookiesCookbook/Program.cs
@@ -1,25 +1,38 @@
-﻿
-FileFormat fileFormat = FileFormat.Txt;
+﻿try
+{
 
 
-IStringRepository stringRepository = fileFormat == FileFormat.Txt ?
-    new StringTextualRepository() :
-    new StringJsonRepository();
-
-IIngredientRegistery ingredientRegistery = new IngredientRegistery();
-
-const string fileName = "recipes";
-
-string fileNameWithExt = new FileMetadata(fileName, fileFormat).ToPath();
+    FileFormat fileFormat = FileFormat.Txt;
 
 
-var recipeRepo =
-    new RecipesRepositroy(stringRepository, ingredientRegistery);
+    IStringRepository stringRepository = fileFormat == FileFormat.Txt ?
+        new StringTextualRepository() :
+        new StringJsonRepository();
 
-var recipeConsoleUserInteraction =
-    new RecipesConsoleUserInteraction(ingredientRegistery);
+    IIngredientRegistery ingredientRegistery = new IngredientRegistery();
 
-var cookiesRecipesApp =
-    new CookiesRecipesApp(recipeRepo, recipeConsoleUserInteraction);
+    const string fileName = "recipes";
 
-cookiesRecipesApp.Run(fileNameWithExt);
+    string fileNameWithExt = new FileMetadata(fileName, fileFormat).ToPath();
+
+
+    var recipeRepo =
+        new RecipesRepositroy(stringRepository, ingredientRegistery);
+
+    var recipeConsoleUserInteraction =
+        new RecipesConsoleUserInteraction(ingredientRegistery);
+
+    var cookiesRecipesApp =
+        new CookiesRecipesApp(recipeRepo, recipeConsoleUserInteraction);
+
+    cookiesRecipesApp.Run(fileNameWithExt);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Sorry! the applicating experienced an " +
+        "unexpected error and will have to be closed." +
+        $"the error message : {ex.Message}");
+
+    Console.WriteLine("Press any key to close.");
+    Console.ReadKey();
+}
