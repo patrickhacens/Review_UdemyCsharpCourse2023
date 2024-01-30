45a62a7779ac39340ca5b0dba2204e157a0ac6f7
Moaad Amer <moaad02@gmail.com>
2024-01-30 21:35:34 +0200

    sunny day scenario

---
+++ 05_GameDataParser/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+Console.WriteLine("Enter the name of the file you want to read:");
+string fileName = Console.ReadLine();
+
+string fileContent = File.ReadAllText(Path.Combine("Resources", fileName));
+
+List<VideoGame> games = JsonSerializer.Deserialize<List<VideoGame>>(fileContent);
+
+if (games.Count > 0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Loaded games are:");
+    foreach (VideoGame game in games)
+    {
+        Console.WriteLine(game);
+    }
+}
+else
+{
+    Console.WriteLine("No games are present in the input file.");
+}
+
+public class VideoGame
+{
+    public string Title { get; set; }
+    public int ReleaseYear { get; set; }
+    public decimal Rating { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Title}, released in {ReleaseYear}, rating: {Rating}";
+    }
+}
