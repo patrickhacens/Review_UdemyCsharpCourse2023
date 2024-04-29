88e711e2a17ab215541c3728f947ce66a4e4ac6d
Moaad Amer <moaad02@gmail.com>
2024-04-29 17:25:57 +0300

    Reducing the size of the dictionaries

---
+++ 07_NET_UnderTheHood/NewSolution/FastRow.cs
@@ -0,0 +1,17 @@
+﻿namespace _07_NET_UnderTheHood.NewSolution
+{
+    public class FastRow
+    {
+        private Dictionary<string, object> _data;
+
+        public FastRow(Dictionary<string, object> data)
+        {
+            _data = data;
+        }
+
+        public object GetAtColumn(string columnName)
+        {
+            return _data.GetValueOrDefault(columnName, null);
+        }
+    }
+}
---
+++ 07_NET_UnderTheHood/NewSolution/FastTableDataBuilder.cs
@@ -0,0 +1,58 @@
+﻿using _07_NET_UnderTheHood.NewSolution;
+using CsvDataAccess.CsvReading;
+using CsvDataAccess.Interface;
+
+namespace CsvDataAccess.NewSolution;
+
+public class FastTableDataBuilder : ITableDataBuilder
+{
+    public ITableData Build(CsvData csvData)
+    {
+        var resultRows = new List<FastRow>();
+
+        foreach (var row in csvData.Rows)
+        {
+            var newRowData = new Dictionary<string, object>();
+
+            for (int columnIndex = 0; columnIndex < csvData.Columns.Length; ++columnIndex)
+            {
+                var column = csvData.Columns[columnIndex];
+                string valueAsString = row[columnIndex];
+                object convertedValue = ConvertValueToTargetType(valueAsString);
+                if (convertedValue != null)
+                {
+                    newRowData[column] = convertedValue;
+                }
+            }
+
+            resultRows.Add(new FastRow(newRowData));
+        }
+
+        return new FastTableData(csvData.Columns, resultRows);
+    }
+
+    private object ConvertValueToTargetType(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return null;
+        }
+        if (value == "TRUE")
+        {
+            return true;
+        }
+        if (value == "FALSE")
+        {
+            return false;
+        }
+        if (value.Contains(".") && decimal.TryParse(value, out var valueAsDecimal))
+        {
+            return valueAsDecimal;
+        }
+        if (int.TryParse(value, out var valueAsInt))
+        {
+            return valueAsInt;
+        }
+        return value;
+    }
+}
