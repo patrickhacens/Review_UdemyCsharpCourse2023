3f7df6c081caec54e31b02ed2a837205990f5ca8
Moaad Amer <moaad02@gmail.com>
2024-06-14 21:03:56 +0300

    fetch the data from api and show in console

---
+++ 08_AdvancedTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using _08_AdvancedTypes.DTO;
+using System.Text.Json;
+
+var starwars = new StarWarsApiDataReader();
+
+string responseContent = await starwars.ReadAsync("https://swapi.dev/api/", "planets");
+
+PlanetsDTO planets = JsonSerializer.Deserialize<PlanetsDTO>(responseContent);
+
+foreach (var item in planets.results)
+{
+    Console.WriteLine($"Name : {item.name}," +
+        $" Diameter : {item.diameter}," +
+        $"SurfaceWater : {item.surface_water}," +
+        $"Population : {item.population}");
+}
+
+
+Console.ReadLine();
+
---
+++ 08_AdvancedTypes/UniversalTablePrinter.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+public class UniversalTablePrinter
+{
+
+    public UniversalTablePrinter(IEnumerable<object> data)
+    {
+        object firstElement = data.First();
+        if (firstElement is not null)
+        {
+            Type type = firstElement.GetType();
+            PropertyInfo[] props = type.GetProperties();
+
+            //print table columns name
+            foreach (PropertyInfo prop in props)
+            {
+                Console.WriteLine($"{prop.Name}-10");
+            }
+
+        }
+
+
+
+
+        //print the data
+
+        //foreach (var item in data)
+        //{
+        //    //item.
+        //}
+
+
+    }
+}
